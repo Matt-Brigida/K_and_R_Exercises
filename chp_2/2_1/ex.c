cfbd4bcc9f8ea7a0a3e39a0c19ba57fa385f44a0
Matt Brigida <mdbrigida@gmail.com>
2021-11-01 10:17:00 -0400

    2-1

---
+++ chp_2/2_1/ex.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+
+  printf("Integers--------\n");
+  printf("The Maximum Integer is %d.\n", INT_MAX);
+  printf("The Miniumum Integer is %d.\n", INT_MIN);
+  printf("Shorts--------\n");
+  printf("The Maximum Short is %d.\n", SHRT_MAX);
+  printf("The Miniumum Short is %d.\n", SHRT_MIN);
+  printf("Chars--------\n");
+  printf("The Maximum Char is %d.\n", CHAR_MAX);
+  printf("The Miniumum Char is %d.\n", CHAR_MIN);
+    
+
+  return 0;
+
+}
+
+
+
+
