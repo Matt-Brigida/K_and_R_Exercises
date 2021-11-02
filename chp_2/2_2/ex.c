ddd54ff9cc873f7b920ededd716f0b6a10f5cebf
Matt Brigida <mdbrigida@gmail.com>
2021-11-01 20:40:45 -0400

    starting 2-2

---
+++ chp_2/2_2/ex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/* for(i=0; i<lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+   s[i] = c;
+
+rewrite the above without using && or || */
+
+int main(){
+
+
+  return 0;
+}
+
+
