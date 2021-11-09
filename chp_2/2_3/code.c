bea044e12a3670d8c81348d0763957efb816c6a6
Matt Brigida <mdbrigida@gmail.com>
2021-11-09 12:22:17 -0500

    added stdlib way

--- chp_2/2_3/code.c
+++ chp_2/2_3/code.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* write int htoi(s) s string of hex digits. */
+
+/* using strtol here is jsut to get an answer to see if my method is correct */
 
 int htoi(char s[])
 {
 
-  int i, n;
-  n = 0;
+  /* put my implementation here */
   
+}
 
+int main(){
+
+  /* const char *my_hex_string = "784abcd1"; */
+  int answer = (int)strtol("784abcd1", NULL, 16);
+  printf("The answer is %d\n", answer);
+  return 0;
   
 }
