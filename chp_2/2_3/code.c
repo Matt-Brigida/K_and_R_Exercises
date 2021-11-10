ccce8040cee817d5ba687232c5f0fdd185f55a97
Matt Brigida <mdbrigida@gmail.com>
2021-11-10 09:37:12 -0500

    segfault

--- chp_2/2_3/code.c
+++ chp_2/2_3/code.c
@@ -8,7 +8,14 @@
 int htoi(char s[])
 {
 
+  int i, n;
+
+  n=0;
+  /* what does converting a letter to int do? We can check first. */
   /* put my implementation here */
+  for (i = 0; ; ++i)
+  printf("the number is %d\n", (int)s[i]);
+  return 1;
   
 }
 
@@ -17,6 +24,7 @@ int main(){
   /* const char *my_hex_string = "784abcd1"; */
   int answer = (int)strtol("784abcd1", NULL, 16);
   printf("The answer is %d\n", answer);
+  htoi("784abcd1");
   return 0;
   
 }
