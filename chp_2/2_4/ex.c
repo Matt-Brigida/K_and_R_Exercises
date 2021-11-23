655d15c3f3d7cade18fea1297fec2fb0e9007a1e
Matt Brigida <mdbrigida@gmail.com>
2021-11-23 18:10:11 -0500

    starting 2-4

---
+++ chp_2/2_4/ex.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+// we could use strchr() however this is likely cheating--------
+
+void new_squeeze(char[] s, char[] w)
+{
+
+  int i, j, k, len;
+  len = sizeof(arr)/sizeof(arr[0]);
+  
+  int match = 0;
+
+  for (i = j = 0, s[i] != '\0', i++)
+    match = 0;
+  for (k = 0, k < len, k++)
+    {
+      if (w[k] == s[i]) match = 1; 
+    }
+
+    
+}
